6101ed0ec424a2cb6e46825e4f7865ffee754d69
oiirn29 <59544576+oiirn29@users.noreply.github.com>
2024-09-12 22:14:10 -0600

    Add files via upload

---
+++ 2.14.1Exercise.py
@@ -0,0 +1,45 @@
+#-------------------------------------------------------------------------------
+# Name:        2.14 exercise 1
+# Purpose:   understanding and using variables
+#
+# Author:Joshua Mazurak
+#
+# Created:09/12/2024
+# Licence: CC BY
+#-------------------------------------------------------------------------------
+
+#Take the sentence: All work and no play makes Jack a dull boy. 
+# Store each word in a separate variable, then print out the 
+# sentence on one line using print.
+
+
+
+al= ("all ")
+
+wo=("work ")
+
+an=("and ")
+
+no=("no ")
+
+pl=("play ")
+
+ma=("makes ")
+
+ja=("jack ")
+
+a=("a ")
+
+du=("dull ")
+
+bo=("boy") 
+
+#lines 17 - 35 are the words in variable form 
+
+sentence= al+wo+an+no+pl+ma+ja+a+du+bo 
+
+#line 39 collates all the variables from 17 -35 into one variable.
+
+print (sentence)
+
+#prints to CMD the variable of sentence
